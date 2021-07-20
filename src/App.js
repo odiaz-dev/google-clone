b670493cc5aad6e4a1356a395bbfe2bbced1b396
Oscar Díaz <oscardiazaed@gmail.com>
2021-07-19 20:30:45 -0400

    clone googe init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Home from './pages/Home';
+import SearchPage from './pages/SearchPage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
+    // BEM
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
+        <Switch>
+          <Route>
+            <SearchPage />
+          </Route>
+          <Route>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+
+      
     </div>
   );
 }
