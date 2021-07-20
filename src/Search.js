d655dcf8b86ce94d1da30594791b41fc8cd07481
Oscar Díaz <oscardiazaed@gmail.com>
2021-07-20 13:20:21 -0400

    prevent function on search component

--- src/Search.js
+++ src/Search.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Search.css';
 import MicIcon from '@material-ui/icons/Mic';
 import SearchIcon from '@material-ui/icons/Search';
 import { Button } from "@material-ui/core";
 
 function Search() {
+
+    const [input, setInput] = useState("");
+
+    const search = e => {
+        e.preventDefault();
+
+        console.log('Hey');
+
+        // do something with input...
+    }
     return (
-        <div className='search'>
+        <form className='search'>
             <div className='search__input'>
                 <SearchIcon className="search__inputIcon" />
-                <input />
+                <input value={input} onChange={e => setInput(e.target.value)}/>
                 <MicIcon />
             </div>
 
             <div className="search__buttons">
-                <Button variant="outlined">Google Search</Button>
+                <Button type="submit" onClick={search} variant="outlined">Google Search</Button>
                 <Button variant="outlined">I'm Feeling Lucky</Button>
             </div>
-        </div>
+        </form>
     )
 }
 
